ce0830c5af937649c3ce39d3dee5592951254058
Mary Gerina <gmary@marys-macbook-pro.local>
2018-10-15 10:03:04 +0300

    Initial Commit

---
+++ TMO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TMO
+//
+//  Created by Mary Gerina on 10/15/18.
+//  Copyright © 2018 Mary Gerina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TMO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TMO
+//
+//  Created by Mary Gerina on 10/15/18.
+//  Copyright © 2018 Mary Gerina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
